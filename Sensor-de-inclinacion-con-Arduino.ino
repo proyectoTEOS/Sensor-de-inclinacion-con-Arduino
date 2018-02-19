28c4e6ea45b4b66b44d07be69f6d5f15a48c22b0
TEOS <proyectoteos@gmail.com>
2018-02-18 23:18:37 -0600

    Code and diagram available
    Program code and armada diagram available

---
+++ Sensor-de-inclinacion-con-Arduino.ino
@@ -0,0 +1,29 @@
+/*
+  Created by TEOS
+  Domotic with Arduino https://goo.gl/btZpjs
+  YouTube https://goo.gl/k7TUSZ
+  Instagram https://goo.gl/zEIjia
+  Facebook https://goo.gl/eivraR
+  Twitter https://goo.gl/du5Wgn
+  Github https://goo.gl/Xl5IiS
+  Google Plus https://goo.gl/gBnUdh
+  WEB https://goo.gl/jtiYJy
+*/
+
+const int sensorPinT = 2;
+int valT;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(sensorPinT, INPUT_PULLUP);
+}
+
+void loop() {
+  valT = digitalRead(sensorPinT);
+  if (valT == LOW) {
+    Serial.println("1er estado del sensor");
+  } else {
+    Serial.println("2do estado del sensor");
+  }
+  delay(1500);
+}
